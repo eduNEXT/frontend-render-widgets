7fec5b0808d6dd19846dd131b9d1d55b8c8c416f
Jose Ignacio Palma <jignaciopm13@gmail.com>
2025-03-14 14:48:17 -0400

    fix: improvements slotwidgetheaderlogo

--- src/frontend-widget-header/SlotWidgetHeaderLogo.jsx
+++ src/frontend-widget-header/SlotWidgetHeaderLogo.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 const SlotWidgetHeaderLogo = () => {
   return (
-    <h1 style={{textAlign: 'center'}}>Modern Theming Logo</h1>
+    <div style={{ 
+      backgroundColor: 'white', 
+      padding: '1rem', 
+      display: 'flex', 
+      justifyContent: 'flex-start', 
+      alignItems: 'center' 
+    }}>
+      <a href="/">
+        <img 
+          className="d-block"
+          src='https://edunextpublic.s3.us-west-2.amazonaws.com/edxsites/sd-files/oddisey-platform/189811072.png'
+          alt="Oddisey Logo"
+        />
+      </a>
+      <p>Oddisey from Modern</p>
+    </div>
   );
 };
 
