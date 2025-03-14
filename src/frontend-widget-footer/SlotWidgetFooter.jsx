2cd46560e0883fddf1a1fda5e8d9b6a16f5c0ed6
Jose Ignacio Palma <jignaciopm13@gmail.com>
2025-03-14 18:38:51 -0400

    fix: styles

--- src/frontend-widget-footer/SlotWidgetFooter.jsx
+++ src/frontend-widget-footer/SlotWidgetFooter.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 
 const SlotWidgetFooter = () => {
   return (
-    <div style={{ 
-      backgroundColor: 'white', 
-      padding: '1rem', 
-      display: 'flex', 
-      justifyContent: 'flex-start', 
-      alignItems: 'center' 
-    }}>
-      <a href="/">
-        <img 
-          className="d-block"
-          src='https://edunextpublic.s3.us-west-2.amazonaws.com/edxsites/sd-files/oddisey-platform/189811072.png'
-          alt="Oddisey Logo"
-        />
-      </a>
-      <p>Oddisey from Modern</p>
-    </div>
+    <footer className='d-flex justify-content-between p-3' style={{ borderTop: '1px solid #ccc' }}>
+      <div className='d-flex justify-content-start align-items-center'>
+        <a href="/">
+          <img
+            style={{ height: '50px' }}
+            className="d-block"
+            src='https://edunextpublic.s3.us-west-2.amazonaws.com/edxsites/sd-files/oddisey-platform/189811072.png'
+            alt="Oddisey Logo"
+          />
+        </a>
+        <div className='pl-3'>
+          <span>Oddisey</span>
+        </div>
+      </div>
+      <div className="d-flex">
+        Footer developed by Modern Theming
+      </div>
+    </footer>
   );
 };
 
--- src/frontend-widget-header/SlotWidgetHeaderLogo.jsx
+++ src/frontend-widget-header/SlotWidgetHeaderLogo.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const SlotWidgetHeaderLogo = () => {
   return (
-    <div style={{ 
-      backgroundColor: 'white', 
-      padding: '1rem', 
-      display: 'flex', 
-      justifyContent: 'flex-start', 
-      alignItems: 'center' 
-    }}>
+    <div
+      className='d-flex justify-content-start align-items-center p-2 mr-2'
+      style={{ borderRight: '1px solid #ccc' }}
+    >
       <a href="/">
-        <img 
+        <img
+          style={{ height: '50px' }}
           className="d-block"
           src='https://edunextpublic.s3.us-west-2.amazonaws.com/edxsites/sd-files/oddisey-platform/189811072.png'
           alt="Oddisey Logo"
         />
       </a>
-      <p>Oddisey from Modern</p>
+      <div className="pl-3">
+        <span>Oddisey Modern</span>
+      </div>
     </div>
   );
 };
